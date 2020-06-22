6b6463395d6e918c8ee7dd6120d814a1a373b972
Victor Luo <victorluo920@gmail.com>
2020-06-22 16:17:39 -0700

    hooked up mongodb atlas with inital models

---
+++ models/topic.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const topicSchema = new Schema(
+  {
+    name: String,
+    description: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+module.exports = mongoose.model('Topic', topicSchema);
--- server.js
+++ server.js
@@ -5,10 +5,18 @@ const logger = require('morgan');
 
 const app = express();
 
+
+require('dotenv').config()
+require('./config/database.js')
+
+
 app.use(logger('dev'));
 app.use(express.json());
+
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api/topics', require('./routes/api/topics'));
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
