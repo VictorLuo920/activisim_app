c300bc1a8ebda422e3dead209e55234dfdc15543
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-06-22 18:27:16 -0700

    finishes step 3

--- src/utils/userService.js
+++ src/utils/userService.js
@@ -1,19 +1,28 @@
+import tokenService from './tokenService';
+
 const BASE_URL = '/api/users/';
 
 function signup(user) {
-  return fetch(BASE_URL + 'signup', {
-    method: 'POST',
-    headers: new Headers({'Content-Type': 'application/json'}),
-    body: JSON.stringify(user)
-  })
-  .then(res => {
-    if (res.ok) return res.json();
-    // Probably a duplicate email
-    throw new Error('Email already taken!');
-  })
-  .then(data => data);
-}
+    return fetch(BASE_URL + 'signup', {
+      method: 'POST',
+      headers: new Headers({'Content-Type': 'application/json'}),
+      body: JSON.stringify(user)
+    })
+    .then(res => {
+      if (res.ok) return res.json();
+      throw new Error('Email already taken!');
+    })
+    // Parameter destructuring!
+      .then(({ token }) => {
+    tokenService.setToken(token);
+  });
+    // the above could have been written as
+    //.then((token) => token.token);
+  }
 
-export default {
-  signup
-};
+  export default {
+    signup,
+    // getUser,
+    // logout,
+    // login
+  }; 
