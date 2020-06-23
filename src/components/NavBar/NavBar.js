347828dbdb55751298269870201d92f59991485a
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-06-22 19:27:46 -0700

    name doesnt go bold on hover in navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,16 +4,16 @@ import './NavBar.css';
 
 const NavBar = (props) => {
     let nav = props.user ?
-        <div className='NavBar-link'>
-            <Link to='' className='NavBar-link' onClick={props.handleLogout} >LOG OUT</Link>
+        <div className='NavBar'>
+            <Link to='' className='NavBar-link' onClick={props.handleLogout} >Log out</Link>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <span className='NavBar-welcome'>WELCOME, {props.user.name}</span>
+            <span className='NavBar-welcome'>Welcome, {props.user.name}</span>
         </div>
         :
         <div className='NavBar'>
-            <Link to="/login" className='NavBar-link'>LOG IN</Link>
+            <Link to="/login" className='NavBar-link'>Log in</Link>
             &nbsp;&nbsp;&nbsp;&nbsp;
-            <Link to="/signup" className='NavBar-link'>SIGN UP</Link>
+            <Link to="/signup" className='NavBar-link'>Sign up</Link>
             &nbsp;&nbsp;&nbsp;&nbsp;
         </div>
 
